e04b7d6511764182f11ddd47fb8f0963e72bed7f
Theodore Moreland <theodoremoreland@yahoo.com>
2021-10-08 09:45:37 -0500

    commented out console.log for now

--- front-end/src/api/fetchMoviePoster.js
+++ front-end/src/api/fetchMoviePoster.js
@@ -21,7 +21,7 @@ export const fetchMoviePoster = async (title, year) => {
             return poster;
         })
         .catch(e => {
-            console.log(e);
+            // console.log(e);
 
             return placeholderPoster;
         });
